6e3de1bd84c6b85201865693410425e1f99b65fa
jagan1470 <78464205+jagan1470@users.noreply.github.com>
2021-09-12 22:52:08 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+function ang1()
+{
+window.location="ang1.html";
+}
+
+
+function pyt1()
+{
+window.location="pyt1.html";
+}
+
+
+function mys1()
+{
+window.location="mys1.html";
+}
+
+
+function ang2()
+{
+window.location="ang1.html";
+}
+
+
+function pyt1()
+{
+window.location="pyt1.html";
+}
+
+function lar()
+{
+window.location="lar.html";
+}
+
+
+function nod1()
+{
+window.location="nod1.html";
+}
+
+
+
+function yii()
+{
+window.location="yii.html";
+}
+
+
+function jav1()
+{
+window.location="jav.html";
+}
+ 
